16fe5e875cb71b5b4a5681e35773e00163d97d5c
Li Yifei <liyf@hdu.edu.com>
2021-03-02 15:19:00 +0800

    api fixed

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,11 +1,11 @@
 public class Planet
 {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img)
     {
