99fde2239f8340828053128ce2172555a655e46c
DeepSpace-Shinku <78863892+DeepSpace-Shinku@users.noreply.github.com>
2021-04-08 15:39:03 +0800

    hw2 completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,141 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import static org.junit.Assert.*;
+
 
 public class Percolation {
+    private int N;
+    private int numOfOpenSites;
+    WeightedQuickUnionUF set;
+    WeightedQuickUnionUF dualSet;
+    boolean opened[][];
 
+    public Percolation(int N)
+    {
+        this.N = N;
+        // let the last node be the bottom node and the one before last be the top node.
+        this.set = new WeightedQuickUnionUF(N * N + 2);
+        this.dualSet = new WeightedQuickUnionUF(N * N + 1);
+        opened = new boolean[N][N];
+        numOfOpenSites = 0;
+    }
+
+    public void open(int row, int col)
+    {
+        opened[row][col] = true;
+        numOfOpenSites += 1;
+        connectAllOpenedAround(row, col);
+        if (row == 0){
+            connectTopToFirstLevel(col);
+        }
+        if (row == N - 1){
+            connectBottomToLastLevel(col);
+        }
+
+    }
+
+    public boolean isOpen(int row, int col)
+    {
+        return opened[row][col];
+    }
+
+    public boolean isFull(int row, int col)
+    {
+        return dualSet.connected(topIndex(), xyTo1D(row, col)) && opened[row][col];
+    }
+
+    public int numberOfOpenSites()
+    {
+        return numOfOpenSites;
+    }
+
+    public boolean percolates()
+    {
+        return set.connected(topIndex(), bottomIndex());
+    }
+
+    private int xyTo1D(int x, int y)
+    {
+        return x + y * N;
+    }
+
+    private void connectAllOpenedAround(int row, int col)
+    {
+        connectIfOpened(row, col, row - 1, col);
+        connectIfOpened(row, col, row, col - 1);
+        connectIfOpened(row, col, row + 1, col);
+        connectIfOpened(row, col, row, col + 1);
+    }
+
+    private void connectIfOpened(int thisRow, int thisCol, int otherRow, int otherCol)
+    {
+        if (otherRow >= 0 && otherRow < N && otherCol >= 0 && otherCol < N && opened[otherRow][otherCol]){
+            set.union(xyTo1D(thisRow, thisCol), xyTo1D(otherRow, otherCol));
+            dualSet.union(xyTo1D(thisRow, thisCol), xyTo1D(otherRow, otherCol));
+        }
+    }
+
+    /**
+     * Return the 1D index of the top Node
+     */
+    private int topIndex()
+    {
+        return xyTo1D(N - 1, N - 1) + 1;
+    }
+
+    /**
+     * Return the 1D index of the bottom Node
+     */
+    private int bottomIndex()
+    {
+        return xyTo1D(N - 1, N - 1) + 2;
+    }
+
+
+    private void connectTopToFirstLevel(int col)
+    {
+        set.union(topIndex(), xyTo1D(0, col));
+        dualSet.union(topIndex(), xyTo1D(0, col));
+    }
+
+    private void connectBottomToLastLevel(int col)
+    {
+        set.union(bottomIndex(), xyTo1D(N - 1, col));
+    }
+
+    public static void main(String[] args)
+    {
+        Percolation p = new Percolation(10);
+        assertFalse(p.percolates());
+        assertEquals(0, p.numberOfOpenSites());
+        p.open(3, 4);
+        assertFalse(p.percolates());
+        assertEquals(1, p.numberOfOpenSites());
+        assertTrue(p.isOpen(3, 4));
+        p.open(0,0);
+        assertFalse(p.percolates());
+        assertEquals(2, p.numberOfOpenSites());
+        p.open(0, 1);
+        assertFalse(p.percolates());
+        assertEquals(3, p.numberOfOpenSites());
+        assertTrue(p.set.connected(p.xyTo1D(0, 0), p.xyTo1D(0, 1)));
+        assertTrue(p.isFull(0, 0));
+        assertTrue(p.isFull(0, 1));
+        assertFalse(p.isFull(1, 1));
+        p.open(1, 1);
+        assertFalse(p.percolates());
+        assertEquals(4, p.numberOfOpenSites());
+        assertTrue(p.isFull(1, 1));
+        assertFalse(p.isFull(9,7));
+        for (int i = 0; i < 9; i++) {
+            p.open(i, 5);
+            assertFalse(p.percolates());
+        }
+        p.open(9, 5);
+        assertTrue(p.percolates());
+        assertFalse(p.isFull(9,7));
+        System.out.println(20 / 5);
+        System.out.println(20 % 5);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,95 @@
 package hw2;
 
+import org.junit.rules.Stopwatch;
+
+import java.util.Random;
+
 public class PercolationStats {
+    private double mean;
+    private double stdDev;
+    private int[] Xs;
+    private int N;
+    private int T;
+    private Random rand;
+
+    public PercolationStats(int N, int T, PercolationFactory pf)
+    {
+        if (N <= 0 || T <= 0){
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        this.T = T;
+        this.rand = new Random();
+        this.Xs = new int[T];
+        for (int i = 0; i < T; i++){
+            Percolation p = pf.make(N);
+            while (!p.percolates()){
+                int index = getNotOpenedIndex(p);
+                p.open(index / N, index % N);
+            }
+            Xs[i] = p.numberOfOpenSites();
+        }
+        calculateMean();
+        calculateStdDev();
+    }
+
+    private void calculateMean()
+    {
+        double result = 0;
+        for (double x: Xs){
+            result += x;
+        }
+        result /= Xs.length;
+        this.mean = result;
+    }
+
+    private void calculateStdDev()
+    {
+        double result = 0;
+        for (double x: Xs){
+            result += (x - mean) * (x - mean);
+        }
+        result /= Xs.length - 1;
+        result = Math.sqrt(result);
+        this.stdDev = result;
+    }
+
+    private int getNotOpenedIndex(Percolation p)
+    {
+        int index =  rand.nextInt(N * N);
+        while(p.isOpen(index / N, index % N)){
+            index = rand.nextInt(N * N);
+        }
+        return index;
+    }
+
+    public double mean()
+    {
+        return this.mean;
+    }
+
+    public double stddev()
+    {
+        return this.stdDev;
+    }
+
+    public double confidenceLow()
+    {
+        return mean - 1.96 * stdDev / Math.sqrt(T);
+    }
+
+    public double confidenceHigh()
+    {
+        return mean + 1.96 * stdDev / Math.sqrt(T);
+    }
+
+    public static void main(String[] args)
+    {
+        PercolationStats ps = new PercolationStats(1000, 100, new PercolationFactory());
+        System.out.println(ps.mean());
+        System.out.println(ps.stddev());
+        System.out.println(ps.confidenceLow());
+        System.out.println(ps.confidenceHigh());
+    }
 
 }
