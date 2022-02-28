acc9afe16820fdb094c5323f02dbd8d51a7dfb58
DeepSpace-Shinku <78863892+DeepSpace-Shinku@users.noreply.github.com>
2022-02-28 09:50:54 +0800

    Remove duplicate files
    Delete proj2/proj2 directory

--- proj2/proj2/byog/Core/Object/GameObject.java
+++
@@ -1,9 +0,0 @@
-package byog.Core.Object;
-
-import byog.TileEngine.TETile;
-import byog.Core.Index;
-
-public interface GameObject {
-    public void addTo(TETile[][] world, Index index);
-    public void remove();
-}
--- proj2/proj2/byog/lab5/RunHexWorld.java
+++
@@ -1,9 +0,0 @@
-package byog.lab5;
-
-import byog.TileEngine.TERenderer;
-import byog.TileEngine.TETile;
-import byog.TileEngine.Tileset;
-
-public class RunHexWorld {
-
-}
